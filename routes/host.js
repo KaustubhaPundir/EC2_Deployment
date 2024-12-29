f3a4e4536e8f0277c564005b098c7908420b202b
Kaustubha Pundir <kaustubhapundir@gmail.com>
2024-12-29 10:58:30 +0530

    added a schema for our collection

--- routes/host.js
+++ routes/host.js
@@ -2,28 +2,31 @@ import path from 'path'; //core module
 import express from 'express';
 import mongoose from 'mongoose';
 const __dirname = path.resolve('.'); //done while using ES module as __dirname, __filename are not available in it
-const hostrouter=express.Router();
+const hostrouter = express.Router();
 
-hostrouter.get('/add-home',(req,res,next)=>{
-    // res.send(`<h1>Add a home</h1>
-    //     <form action='/host/add-home' method='POST'>
-    //         <input type='text' placeholder='enter the home name' name='housename'/>
-    //         <input type='submit'/>
-    //     </form>`);
-    res.sendFile(path.join(__dirname,'../','Proj_Deploy','views','addHome.html')); //file is send
+hostrouter.get('/add-home', (req, res, next) => {
+  // res.send(`<h1>Add a home</h1>
+  //     <form action='/host/add-home' method='POST'>
+  //         <input type='text' placeholder='enter the home name' name='housename'/>
+  //         <input type='submit'/>
+  //     </form>`);
+  res.sendFile(path.join(__dirname, '../', 'Proj_Deploy', 'views', 'addHome.html')); //file is send
 });
-hostrouter.post('/host/add-home',(req,res,next)=>{
-    // res.send(`<h1>Home registered successfully</h1>
-    //     <a href="/">Go back to homepage</a>`);
-    mongoose.connect(
-      "mongodb://0.0.0.0:27017/testdb",
-      {
-          useNewUrlParser: true,
-          useUnifiedTopology: true
-      });
-  
-    res.sendFile(path.join(__dirname,'../','Proj_Deploy','views','homeaddedsuccess.html')); //file is send
-    console.log(req.body);
+hostrouter.post('/host/add-home', (req, res, next) => {
+  // res.send(`<h1>Home registered successfully</h1>
+  //     <a href="/">Go back to homepage</a>`);
+  mongoose.connect(
+    "mongodb://0.0.0.0:27017/testdb",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+  const contactSchema = {
+    email: String,
+    query: String,
+  };
+  res.sendFile(path.join(__dirname, '../', 'Proj_Deploy', 'views', 'homeaddedsuccess.html')); //file is send
+  console.log(req.body);
 })
 
 export default hostrouter;
